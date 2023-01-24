b94884e13dd362be3187dfce78e2126fe9fc957c
Denis <domino387@gmail.com>
2023-01-24 13:42:40 +0300

    add task3_009 json-server

--- Advanced/js/script.js
+++ Advanced/js/script.js
@@ -297,4 +297,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
